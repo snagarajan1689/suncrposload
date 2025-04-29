34af19eaca5634ebc95d6a281521e1168797ab33
snagarajan1689 <127357388+snagarajan1689@users.noreply.github.com>
2025-04-29 12:51:23 -0400

    Delete src directory

--- src/functions/storageBlobTrigger1.js
+++
@@ -1,9 +0,0 @@
-const { app } = require('@azure/functions');
-
-app.storageBlob('storageBlobTrigger1', {
-    path: 'samples-workitems/{name}',
-    connection: '',
-    handler: (blob, context) => {
-        context.log(`Storage blob function processed blob "${context.triggerMetadata.name}" with size ${blob.length} bytes`);
-    }
-});
